d4e3ec443edf2ae03ddde9cc6144d6a47b1859f8
Lalit Kapoor <lalitkapoor@gmail.com>
2012-12-21 11:16:25 -0600

    added support for staging and production environments. Also cleaned up environment to have a defaultconfiguration from which all other environment extend

--- config.js
+++ config.js
@@ -1,59 +1,61 @@
+
+/**
+ * Module dependencies
+ */
+
+var
+  os = require('os')
+, _ = require('lodash')
+;
+
 var config = {
-  dev: {
-    postgresConnStr:  "postgres://localhost:5432/goodybag"
-  , cookieSecret: "g00dybagr0cks!"
-  , numWorkers: 4
+  defaults: {
+    http: {
+      port: 3000
+    }
   , repl: {
       prompt: "gb-api> "
     , port: 4337
     }
-  , http: {
-      port: 3000
-    }
-  , validationOptions: {
-      singleError: false
-    }
+  , numWorkers: os.cpus().length
+  , cookieSecret: "g00dybagr0ck$!"
   , facebook: {
       id: "159340790837933"
     , openGraphUrl: "https://graph.facebook.com/"
     }
-
   , passwordSalt: "$G00DYBAGR0CK$!"
   , consumerPasswordSaltLength: 10
+  , validationOptions: {
+      singleError: false
+    }
+  }
+
+, dev: {
+    http: {
+      port: 3000
+    }
+  , postgresConnStr:  "postgres://localhost:5432/goodybag"
   }
 
 , test: {
-    postgresConnStr:  "postgres://localhost:5432/goodybag-test"
-  , cookieSecret: "g00dybagr0cks!"
-  , numWorkers: 4
-  , repl: {
-      prompt: "gb-api> "
-    , port: 4337
-    }
-  , http: {
+    http: {
       port: 8986
     }
-  , validationOptions: {
-      singleError: false
-    }
-  , facebook: {
-      id: "159340790837933"
-    , openGraphUrl: "https://graph.facebook.com/"
-    }
-
-  , passwordSalt: "$G00DYBAGR0CK$!"
-  , consumerPasswordSaltLength: 10
+  , postgresConnStr:  "postgres://localhost:5432/goodybag-test"
   , baseUrl: "http://localhost:8986"
   }
 
+, staging: {
+    postgresConnStr: process.env['DATABASE_URL']
+  }
 , production: {
 
   }
 };
 
-for (var key in config.dev){
-  config.production[key] = config.dev;
-}
 
-module.exports = config[process.env.mode || 'dev'];
-console.log('Loading '+(process.env.mode || 'dev')+' config');
+var mode = process.env['GB_ENV'] || 'dev';
+if (mode == null || !config.hasOwnProperty(mode)) mode = 'dev';
+
+module.exports = _.extend(config.defaults, config[mode]);
+console.log('Loading ' + mode + ' config');
