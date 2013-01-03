a030a2d201d7b3b5ce295e3cf99c5670ee4210aa
Linh Vo <linhvo@goodybag>
2013-01-03 08:45:43 -0600

    revert test user

--- test/collections/users.js
+++ test/collections/users.js
@@ -67,19 +67,10 @@ describe('POST /v1/users', function() {
 
   it('should not create new user if user email existing', function(done){
 
-    before(function(){
-      var user = {
-        email: "tests@goodybag.com"
-      , password: "password"
-      };
-    utils.post(baseUrl + '/v1/users', user, function(error, res,payload){
-
-    })
-    })
     var user = {
-      email: "tests@goodybag.com"
+      email: "sales@goodybag.com"
     , password: "password"
-    };
+    }
 
     utils.post(baseUrl + '/v1/users', user, function(error, res, payload){
       assert(!error);
