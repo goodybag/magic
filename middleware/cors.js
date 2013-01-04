a9fd03513683c3f377ec544dab0383359fdd6973
Lalit Kapoor <lalitkapoor@gmail.com>
2013-01-04 10:05:59 -0600

    allow requested headers

--- middleware/cors.js
+++ middleware/cors.js
@@ -5,12 +5,13 @@
 var corsHeaders = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin',      req.headers['origin']); // req.headers['origin'] is necessary to get authorization to work
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods',     'OPTIONS, HEAD, GET, PUT, PATCH, POST, DELETE');
-  res.setHeader('Access-Control-Allow-Headers',     'Content-type, Authorization');
-  res.setHeader('Access-Control-Expose-Headers',    'Allow, Link, Content-type');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, PUT, PATCH, POST, DELETE');
+  //res.setHeader('Access-Control-Allow-Headers', 'Accept, Content-type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+  res.setHeader('Access-Control-Expose-Headers','Allow, Link, Content-type');
 
   // intercept OPTIONS method, this needs to respond with a zero length response (pre-flight for CORS).
-  if (req.method == 'OPTIONS') { return res.send(200); }
+  if (req.method === 'OPTIONS') return res.send(200);
   next();
 };
 module.exports = corsHeaders;
