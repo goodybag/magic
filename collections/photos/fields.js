7373c554e88bfe432ced0df9e58d1c162eff3b01
Paul Frazee <paulfrazee@goodybag.com>
2013-01-11 11:40:12 -0600

    fixed photo update routes

--- collections/photos/fields.js
+++ collections/photos/fields.js
@@ -3,63 +3,119 @@ var photos = db.tables.photos;
 var products = db.tables.products;
 
 module.exports = {
-  "default": {
-    id          : photos.id
-  , businessId  : photos.businessId
-  , productId   : photos.productId
-  , productName : products.name
-  , consumerId  : photos.consumerId
-  , url         : photos.url
-  , notes       : photos.notes
-  , lat         : photos.lat
-  , lon         : photos.lon
-  , isEnabled   : photos.isEnabled
-  , createdAt   : photos.createdAt
-  , $postRequires : ['url']
-  }
+  get: {
+    "default": {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , productName : products.name
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    }
 
-, client: {
-    id          : photos.id
-  , businessId  : photos.businessId
-  , productId   : photos.productId
-  , productName : products.name
-  , consumerId  : photos.consumerId
-  , url         : photos.url
-  , notes       : photos.notes
-  , lat         : photos.lat
-  , lon         : photos.lon
-  , isEnabled   : photos.isEnabled
-  , createdAt   : photos.createdAt
-  , $postRequires : ['url']
-  }
+  , client: {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , productName : products.name
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    }
 
-, sales: {
-    id          : photos.id
-  , businessId  : photos.businessId
-  , productId   : photos.productId
-  , productName : products.name
-  , consumerId  : photos.consumerId
-  , url         : photos.url
-  , notes       : photos.notes
-  , lat         : photos.lat
-  , lon         : photos.lon
-  , isEnabled   : photos.isEnabled
-  , createdAt   : photos.createdAt
-  , $postRequires : ['url']
-  }
+  , sales: {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , productName : products.name
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    }
 
-, admin: {
-    id          : photos.id
-  , businessId  : photos.businessId
-  , productId   : photos.productId
-  , productName : products.name
-  , consumerId  : photos.consumerId
-  , url         : photos.url
-  , notes       : photos.notes
-  , lat         : photos.lat
-  , lon         : photos.lon
-  , isEnabled   : photos.isEnabled
-  , createdAt   : photos.createdAt
-  , $postRequires : ['url']
+  , admin: {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , productName : products.name
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    }
+  },
+  modify: {
+    "default": {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    , $postRequires : ['url']
+    }
+
+  , client: {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    , $postRequires : ['url']
+    }
+
+  , sales: {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    , $postRequires : ['url']
+    }
+
+  , admin: {
+      id          : photos.id
+    , businessId  : photos.businessId
+    , productId   : photos.productId
+    , consumerId  : photos.consumerId
+    , url         : photos.url
+    , notes       : photos.notes
+    , lat         : photos.lat
+    , lon         : photos.lon
+    , isEnabled   : photos.isEnabled
+    , createdAt   : photos.createdAt
+    , $postRequires : ['url']
+    }
+
   }
 };
