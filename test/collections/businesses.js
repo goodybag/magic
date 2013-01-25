4ec54dcb636718c86fe007d5d6b122b3393ca58c
John Fawcett <jrf0110@gmail.com>
2013-01-25 09:59:16 -0600

    added more tests

--- test/collections/businesses.js
+++ test/collections/businesses.js
@@ -207,6 +207,41 @@ describe('PATCH /v1/businesses/:id', function(){
     });
   });
 
+  it('should update a business\'s url to be blank', function(done){
+    var business = {
+      url: null
+    };
+
+    tu.loginAsSales(function(error, user){
+      tu.patch('/v1/businesses/' + 1, business, function(error, results, res){
+        assert(!error);
+        assert(res.statusCode == 200);
+        tu.logout(function(){
+          done();
+        });
+      });
+    });
+  });
+
+  it('should fail to update a business\'s url to be blank because of a validation error', function(done){
+    var business = {
+      url: ""
+    };
+
+    tu.loginAsSales(function(error, user){
+      tu.patch('/v1/businesses/' + 1, business, function(error, results, res){
+        assert(!error);
+        assert(res.statusCode == 400);
+        results = JSON.parse(results);
+        assert(results.error);
+        assert(results.error.name === "VALIDATION_FAILED");
+        tu.logout(function(){
+          done();
+        });
+      });
+    });
+  });
+
   it('should update a business\'s tags', function(done){
     var business = {
       tags:['foo','bar','baz']
