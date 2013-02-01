84ca2f474bc52580efe5ab327b17554e9f03e48e
Paul Frazee <paulfrazee@goodybag.com>
2013-02-01 10:11:25 -0600

    added file that was missed when photo perms were updated

---
+++ collections/photos/apply-groups.js
@@ -0,0 +1,61 @@
+/**
+ * Owner permissions
+ */
+
+var
+  db  = require('../../db')
+, sql = require('../../lib/sql')
+;
+
+exports.owner = function(req, cb) {
+  if (!req.session || !req.session.user) return cb(null);
+  if (req.session.user.groups.indexOf('consumer') === -1) return cb(null);
+
+  var userId = req.session.user.id;
+
+  db.getClient(function(error, client){
+    if (error) return cb(null);
+    
+    var query = sql.query([
+      'SELECT 1 FROM photos',
+        'INNER JOIN consumers ON',
+          'consumers.id = photos."consumerId" AND',
+          'consumers."userId" = $userId',
+        'WHERE photos.id = $photoId'
+    ]);
+    query.$('photoId', req.param('photoId'));
+    query.$('userId', userId);
+    client.query(query.toString(), query.$values, function(err, results) {
+      if (err || results.rowCount === 0) return cb(null);
+      cb('owner');
+    });
+  });
+};
+
+exports.businessOwner = function(req, cb) {
+  if (!req.session || !req.session.user) return cb(null);
+
+  var isManager = (req.session.user.groups.indexOf('manager') !== -1);
+  if (!isManager && req.session.user.groups.indexOf('cashier') === -1) return cb(null);
+
+  var userId = req.session.user.id;
+
+  db.getClient(function(error, client){
+    if (error) return cb(null);
+    
+    var query = sql.query([
+      'SELECT 1, {userTable}."businessId" FROM photos',
+        'INNER JOIN {userTable} ON',
+          '{userTable}."businessId" = photos."businessId" AND',
+          '{userTable}."userId" = $userId',
+        'WHERE photos.id = $photoId'
+    ]);
+    query.userTable = (isManager) ? 'managers' : 'cashiers';
+    query.$('photoId', req.param('photoId'));
+    query.$('userId', userId);
+    client.query(query.toString(), query.$values, function(err, results) {
+      if (err || results.rowCount === 0) return cb(null);
+      cb('businessOwner');
+    });
+  });
+};
