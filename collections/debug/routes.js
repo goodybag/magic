f14503e15e2cf094b2b4a3f5472d99da51fcb802
Paul Frazee <paulfrazee@goodybag.com>
2013-02-01 14:50:35 -0600

    improvements to profile output

--- collections/debug/routes.js
+++ collections/debug/routes.js
@@ -11,37 +11,37 @@ module.exports.profile = function(req, res){
       res.json(data);
     },
     'text/html':function() {
-      var netLocalAvg = data.totals.reduce(function(acc, total) { return acc + total.localAverage }, 0);
+      var netTotalAvg = data.tree.children.reduce(function(acc, node) { return acc + node.totalAverage }, 0);
 
       res.end([
         '<h1>Route Totals</h1>',
         '<table>',
-          '<tr><th>Name</th><th>Elapsed</th><th>Calls</th><th>Avg</th><th>% of All</th><th>% of Avg</th></tr>',
+          '<tr><th>Name</th><th>Elapsed</th><th>Calls</th><th>% of All</th><th>Avg</th><th>% of Avg</th></tr>',
           data.totals.filter(routes).map(function(total) {
             return [
               '<tr>',
                 '<td>', total.name, '</td>',
                 '<td>', total.totalElapsedf, '</td>',
                 '<td>', total.count, '</td>',
-                '<td>', total.totalAveragef, '</td>',
                 '<td>', Math.round(total.totalPercent.ofAll), '%</td>',
-                '<td>', Math.round(100 * total.totalAverage / netLocalAvg), '%</td>',
+                '<td>', total.totalAveragef, '</td>',
+                '<td>', Math.round(100 * total.totalAverage / netTotalAvg), '%</td>',
               '</tr>'
             ].join('');
           }).join(''),
         '</table>',
         '<h1>Function Totals</h1>',
         '<table>',
-          '<tr><th>Name</th><th>Elapsed</th><th>Calls</th><th>Avg</th><th>% of All</th><th>% of Avg</th></tr>',
+          '<tr><th>Name</th><th>Elapsed</th><th>Calls</th><th>% of All</th><th>Avg</th><th>% of Avg</th></tr>',
           data.totals.filter(functions).map(function(total) {
             return [
               '<tr>',
                 '<td>', total.name, '</td>',
                 '<td>', total.localElapsedf, '</td>',
                 '<td>', total.count, '</td>',
-                '<td>', total.localAveragef, '</td>',
                 '<td>', Math.round(total.localPercent.ofAll), '%</td>',
-                '<td>', Math.round(100 * total.localAverage / netLocalAvg), '%</td>',
+                '<td>', total.localAveragef, '</td>',
+                '<td>', Math.round(100 * total.localAverage / netTotalAvg), '%</td>',
               '</tr>'
             ].join('');
           }).join(''),
