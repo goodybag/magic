2e9de3e7a20e691b37d2b3b169adb2c39f30178a
Paul Frazee <paulfrazee@goodybag.com>
2013-02-07 18:06:13 -0600

    added db/procedures.js (missed earlier)

---
+++ db/procedures.js
@@ -0,0 +1,78 @@
+
+/**
+ * Consumer punch logic
+ *
+ * @param  object   client
+ * @param  object   tx            Transaction
+ * @param  int      consumerId
+ * @param  int      businessId
+ * @param  int      deltaPunches
+ * @param  function cb            Called with (error, hasEarnedReward, numRewards, hasEarnedElite, dateEliteEarned)
+ * @return undefined
+ */
+module.exports.updateUserLoyaltyStats = function(client, tx, consumerId, businessId, deltaPunches, cb) {
+
+  // run stats upsert
+  db.upsert(client,
+    ['UPDATE "userLoyaltyStats"',
+      'SET',
+        '"numPunches"   = "numPunches"   + $3,',
+        '"totalPunches" = "totalPunches" + $3',
+      'WHERE "consumerId"=$1 AND "businessId"=$2 RETURNING id, "numPunches", "totalPunches", "isElite", "numRewards"'
+    ].join(' '),
+    [consumerId, businessId, deltaPunches],
+    ['INSERT INTO "userLoyaltyStats"',
+      '("consumerId", "businessId", "numPunches", "totalPunches", "numRewards", "visitCount", "lastVisit", "isElite")',
+      'SELECT $1, $2, $3, $3, 0, 0, now(), false WHERE NOT EXISTS',
+        '(SELECT 1 FROM "userLoyaltyStats" WHERE "consumerId"=$1 AND "businessId"=$2) RETURNING id, "numPunches", "totalPunches", "isElite", "numRewards"'
+    ].join(' '),
+    [consumerId, businessId, deltaPunches],
+    tx, // provide a transaction
+    function(error, result) {
+      if (error) return cb(error);
+      var uls = result.rows[0];
+
+      // Look up the business loyalty settings
+      tx.query('SELECT * from "businessLoyaltySettings" where "businessId" = $1', [businessId], function(error, result) {
+          if (error) return cb(error);
+          var bls = result.rows[0];
+
+          var query = sql.query([
+            'UPDATE "userLoyaltyStats" SET {updates}',
+              'WHERE "consumerId"=$consumerId AND "businessId"=$businessId',
+              'RETURNING "numRewards", "dateBecameElite"'
+          ]);
+          query.updates = sql.fields();
+          query.$('consumerId', consumerId);
+          query.$('businessId', businessId);
+
+          var hasBecomeElite = (uls.totalPunches >= bls.punchesRequiredToBecomeElite);
+          if (hasBecomeElite) {
+            query.updates.add('"isElite" = true');
+            query.updates.add('"dateBecameElite"  = now()');
+          }
+       
+          var punchesRequired = (uls.isElite) ? bls.elitePunchesRequired : bls.regularPunchesRequired;
+          var hasEarnedReward = (uls.numPunches >= punchesRequired);
+          var rewardsGained = 0;
+          if (hasEarnedReward) {
+            rewardsGained = Math.floor(uls.numPunches / punchesRequired); // this accounts for the possibility that they punched more than the # required
+            query.updates.add('"numPunches" = "numPunches" - $punchesRequired');
+            query.updates.add('"numRewards" = "numRewards" + $rewardsGained');
+            query.$('punchesRequired', punchesRequired * rewardsGained);
+            query.$('rewardsGained', rewardsGained);
+          }
+
+          if (!hasEarnedReward && !hasBecomeElite)
+            return cb(null, false, uls.numRewards, false, null);
+
+          // Update the users loyalty stat with elite
+          tx.query(query.toString(), query.$values, function(error, result) {
+            if (error) return cb(error);
+            cb(null, hasEarnedReward, result.rows[0].numRewards, hasBecomeElite, result.rows[0].dateBecameElite);
+          });
+        }
+      );
+    }
+  );
+};
