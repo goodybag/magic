99b9813105ed2a476c939e854d35eedaee533f9d
Lalit Kapoor <lalitkapoor@gmail.com>
2013-02-08 01:42:32 -0600

    damn it, the url was hard

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ var config = {
   , http: {
       port: 8986
     }
-  , postgresConnStr:  "postgres://localhost:5432/goodybag-test"
+  , postgresConnStr:  process.env['PG_CON'] || "postgres://localhost:5432/goodybag-test"
   , baseUrl: "http://localhost:8986"
 
   , facebook: {
