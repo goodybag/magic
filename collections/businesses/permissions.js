c414198212b6878df7efb6ed0e7cfeaade0a26a2
Paul Frazee <paulfrazee@goodybag.com>
2013-03-04 09:41:48 -0600

    fixed typo in business permissions

--- collections/businesses/permissions.js
+++ collections/businesses/permissions.js
@@ -1,6 +1,6 @@
 module.exports.business = {
   world: {
-    read:   ['id', 'name', 'locations:locations', 'tags', 'isGb']
+    read:   ['id', 'name', 'locations:locations', 'tags', 'isGB']
   , create: []
   , update: []
   }
