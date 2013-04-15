9cea6e829d162338eecdfea641da76021aa32f92
Geoffrey Parker <grp@gparker.co>
2013-04-15 18:20:44 -0500

    started schema for new table

---
+++ db/schemas/requests.js
@@ -0,0 +1,40 @@
+/**
+ * Requests schema
+ */
+
+if (typeof module === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+define(function(require){
+  var consumers = {
+    uuid: {
+      type: 'text'
+    , meta: 'primary key'
+    }
+  , userId: {
+      type: 'int'
+    , meta: 'references users(id)'
+    , validators: { isInt:[] }
+    }
+  , method: {
+      type: 'text'
+    , sanitizers: { trim:[] }
+    }
+  , url: {
+      type: 'text'
+    , sanitizers: { trim:[] }
+    }
+  , application: {
+      type: 'text'
+    , sanitizers: { trim:[] }
+    }
+  , userAgent: {
+      type: 'text'
+    , sanitizers: { trim:[] }
+    }
+  };
+  return consumers;
+});
