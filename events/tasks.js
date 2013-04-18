6c0d72cba8e1089e26a6888019a96f2d17686168
Geoffrey Parker <grp@gparker.co>
2013-04-18 14:39:01 -0500

    update user loyalty stats when number of punches required drops

--- events/tasks.js
+++ events/tasks.js
@@ -5,6 +5,7 @@
 var
   db        = require('../db')
 , utils     = require('../lib/utils')
+, sql       = require('../lib/sql')
 , magic     = require('../lib/magic')
 , config    = require('../config')
 , templates = require('../templates')
@@ -47,5 +48,41 @@ module.exports = {
     , 'Business Contacted Us'
     , JSON.stringify(info, true, '  ')
     );
+  }
+
+, 'loyalty.settingsUpdate':
+  function (id, body, result) {
+    var changes = result.rows[0];
+
+    var eliteChanged = changes.new_elite < changes.old_elite;
+    var regularChanged = changes.new_regular < changes.old_regular;
+
+    if (!eliteChanged && !regularChanged) return;
+
+
+    var query = sql.query('UPDATE "userLoyaltyStats" SET '
+      + '"numRewards" = "numRewards" + ("numPunches" / (CASE WHEN "isElite" IS TRUE THEN {eliteRequired} ELSE {regularRequired} END)), '
+      + '"numPunches" = "numPunches" % (CASE WHEN "isElite" IS TRUE THEN {eliteRequired} ELSE {regularRequired} END) '
+      + '{where}');
+
+    query.where = sql.where('"businessId" = {id}');
+    if (eliteChanged !== regularChanged)
+      query.where = query.where.and('"isElite" IS ' + (eliteChanged ? 'TRUE' : 'FALSE'));
+
+    console.log('query where: ', query.where);
+
+    query.id = id;
+
+    query.eliteRequired = changes.new_elite;
+    query.regularRequired = changes.new_regular;
+
+    console.log("query: ", query.toString());
+    console.log("values: ", query.$values);
+
+    var TAGS = ['loyalty-settings-update'];
+    db.getClient(TAGS, function(error, client){
+      if (error) return;
+      client.query(query.toString(), query.$values, function(error, result){});
+    });
   }
 };
