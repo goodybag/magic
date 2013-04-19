c069a9a958b31bf01b1e4335f7cf0d6666f91185
Geoffrey Parker <grp@gparker.co>
2013-04-19 12:44:09 -0500

    removed consumers that are not users.  tests still passing

--- db/deltas/1.0.25.sql
+++ db/deltas/1.0.25.sql
@@ -3,7 +3,11 @@ insert into deltas (version, date) values ('1.0.25', 'now()');
 
 -- #503 - Missing some database constraints on consumers, managers, and cachiers
 
+-- remove consumers that don't have a cooresponding user
+
+DELETE FROM consumers WHERE id NOT IN (SELECT id from users);
+
 -- This adds foreign key and delete cascade constraints to the three tables that are missing them
---ALTER TABLE consumers ADD CONSTRAINT consumers_id_fkey FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE; -- consumers has bad data.  don't wan't to delete without being sure it's not real.
+ALTER TABLE consumers ADD CONSTRAINT consumers_id_fkey FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE;
 ALTER TABLE managers ADD CONSTRAINT managers_id_fkey FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE;
 ALTER TABLE cashiers ADD CONSTRAINT cashiers_id_fkey FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE;
