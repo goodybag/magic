ae2047ebcff3bb82d987e5a94746e501c8db5380
bmc <brian.m.carlson@gmail.com>
2013-04-22 18:51:49 -0500

    close connection on error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,10 +4,9 @@ var log = require(__dirname + '/../lib/logger')({app: 'api', component: 'errorHa
 var errorHandler = module.exports = function(app) {
   return function(err, req, res, next) {
     delete err.domain;
-    log.error('unhandled exception', err);
     //respond kindly to the offender if possible
     try {
-      res.writeHead(500, {'content-type':'application/json'});
+      res.writeHead(500, {'content-type':'application/json', 'connection': 'close'});
       res.end(JSON.stringify({error: errors.internal.UNKNOWN, data: null}));
     } catch(e) {
       delete e.domain;
