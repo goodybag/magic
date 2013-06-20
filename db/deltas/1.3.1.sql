645dd7f15df03381cf119bb4ae0abd5f168874dc
Geoffrey Parker <grp@gparker.co>
2013-06-20 13:58:13 -0500

    added phone column to users

---
+++ db/deltas/1.3.1.sql
@@ -0,0 +1,6 @@
+-- 1.3.1.sql
+insert into deltas (version, date) values ('1.3.1', 'now()');
+
+-- #625 - Auth by phone number
+
+ALTER TABLE users ADD COLUMN phone text CHECK (phone SIMILAR TO '\d{10}(ex\d{4})?');
