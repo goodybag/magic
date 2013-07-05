e6701a11037902e6531f570f0db543dfa63447ef
John Fawcett <jrf0110@gmail.com>
2013-07-05 12:00:43 -0500

    add if not exists to table creation statements

--- db/copper/deltas/1.0.0.sql
+++ db/copper/deltas/1.0.0.sql
@@ -9,57 +9,57 @@ create table if not exists requests (
 );
 
 -- Dump all of the old requests into this massive partion
-create table requests_20130701 (
+create table if not exists requests_20130701 (
   check ( "createdAt" < '2013-06-01' )
 ) inherits (requests);
 
 -- Create enough new requests tables to last us a while
 
-create table requests_20130701_20130801 (
+create table if not exists requests_20130701_20130801 (
   check ( "createdAt" >= date '2013-07-01' and "createdAt" < date '2013-08-01' )
 ) inherits (requests);
 
-create table requests_20130801_20130901 (
+create table if not exists requests_20130801_20130901 (
   check ( "createdAt" >= date '2013-08-01' and "createdAt" < date '2013-09-01' )
 ) inherits (requests);
 
-create table requests_20130901_20131001 (
+create table if not exists requests_20130901_20131001 (
   check ( "createdAt" >= date '2013-09-01' and "createdAt" < date '2013-10-01' )
 ) inherits (requests);
 
-create table requests_20131001_20131101 (
+create table if not exists requests_20131001_20131101 (
   check ( "createdAt" >= date '2013-10-01' and "createdAt" < date '2013-11-01' )
 ) inherits (requests);
 
-create table requests_20131101_20131201 (
+create table if not exists requests_20131101_20131201 (
   check ( "createdAt" >= date '2013-11-01' and "createdAt" < date '2013-12-01' )
 ) inherits (requests);
 
-create table requests_20131201_20140101 (
+create table if not exists requests_20131201_20140101 (
   check ( "createdAt" >= date '2013-12-01' and "createdAt" < date '2014-01-01' )
 ) inherits (requests);
 
-create table requests_20140101_20140201 (
+create table if not exists requests_20140101_20140201 (
   check ( "createdAt" >= date '2014-01-01' and "createdAt" < date '2014-02-01' )
 ) inherits (requests);
 
-create table requests_20140201_20140301 (
+create table if not exists requests_20140201_20140301 (
   check ( "createdAt" >= date '2014-02-01' and "createdAt" < date '2014-03-01' )
 ) inherits (requests);
 
-create table requests_20140301_20140401 (
+create table if not exists requests_20140301_20140401 (
   check ( "createdAt" >= date '2014-03-01' and "createdAt" < date '2014-04-01' )
 ) inherits (requests);
 
-create table requests_20140401_20140501 (
+create table if not exists requests_20140401_20140501 (
   check ( "createdAt" >= date '2014-04-01' and "createdAt" < date '2014-05-01' )
 ) inherits (requests);
 
-create table requests_20140501_20140601 (
+create table if not exists requests_20140501_20140601 (
   check ( "createdAt" >= date '2014-05-01' and "createdAt" < date '2014-06-01' )
 ) inherits (requests);
 
-create table requests_20140601_20140701 (
+create table if not exists requests_20140601_20140701 (
   check ( "createdAt" >= date '2014-06-01' and "createdAt" < date '2014-07-01' )
 ) inherits (requests);
 
