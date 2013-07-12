c082069ec6283aee4389c0aeb9778567afe1e292
John Fawcett <jrf0110@gmail.com>
2013-07-12 15:48:20 -0500

    WIP bin update search index with data

--- bin/update-elastic-search.js
+++ bin/update-elastic-search.js
@@ -1,28 +1,61 @@
 #!/usr/bin/env node
 
+var util    = require('util');
 var async   = require('async');
 var db      = require('../db');
 var elastic = require('../lib/elastic-search');
 
+var options = {
+  rateLimit: 50
+}
+
 var productQueryOptions = {
-  order: 'id asc'
+  fields: ['products.*', 'businesses.name as businessName']
+
+, $leftJoin: {
+    businesses: {
+      'businesses.id': 'products.businessId'
+    }
+  }
 };
 
-db.products.find({}, productQueryOptions, function(error, products){
+var stats = {
+  'Number Completed': 0
+};
+
+var outputStats = function(){
+  console.log("\n\n")
+  for (var key in stats){
+    console.log(key, stats[key]);
+  }
+  console.log("\n\n")
+};
+
+var complete = function(error){
+  outputStats();
+  if (error) throw error;
+  process.exit(0)
+}
+
+console.log("Finding Products");
+
+db.api.products.find({}, productQueryOptions, function(error, products){
   if (error) throw error;
 
-  async.parallel(
+  console.log("Found", products.length, "products");
+  console.log("\n\n");
+
+  async.series(
     products.map(function(product){
       return function(done){
-        elastic.save(product, done);
+        elastic.save('product', product, function(error){
+          if (error) return done(error);
+          util.print('.');
+          stats['Number Completed']++;
+          done();
+        });
       };
     })
-  , function(error){
-
-    }
+  , complete
   );
-
-  products.forEach(function(product){
-    db.elastic.save('product', product)
-  });
 });
